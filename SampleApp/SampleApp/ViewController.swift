806fabce17ad802e69efda9828ffe6d6554631b3
Chaehui Seo <sch0991@naver.com>
2024-03-10 22:18:03 +0900

    [FEAT] Add sampleApp project

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by 서채희 on 2024/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
